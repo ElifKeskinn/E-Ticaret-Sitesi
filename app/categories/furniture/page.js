a9e385795b58d8b9a91f2716cf0974dc6325f47f
Elif <elif.keskin233@gmail.com>
2024-09-27 16:47:53 +0300

    module css imported to all category-product pages

--- app/categories/furniture/page.js
+++ app/categories/furniture/page.js
@@ -1,13 +1,15 @@
 import ProductCard from '../../components/ProductCard';
+import styles from '../CategoryPage.module.css'
+
 
 export default async function FurniturePage() {
   const res = await fetch('https://dummyjson.com/products/category/furniture');
   const data = await res.json();
 
   return (
-    <div className='category-title'>
+    <div className={styles['category-title']}>
       <h2>Furniture Products</h2>
-      <div className='category-products'>
+      <div className={styles['category-products']}>
         {data.products.map(product => (
           <ProductCard key={product.id} product={product} />
         ))}
