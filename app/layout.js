8422b3b500555369d2561b1566a2c4afd9f37010
Elif <elif.keskin233@gmail.com>
2024-09-27 14:36:39 +0300

    navbar moved to home page

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "./components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -22,7 +21,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-      <Navbar />
         {children}
       </body>
     </html>
